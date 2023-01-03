8caee6f4cef99cbcacacabcc2db53348795dbcc9
ilisien <ian8lisien@gmail.com>
2023-01-03 00:05:11 -0500

    error pages

--- dorsal/dorsal/urls.py
+++ dorsal/dorsal/urls.py
@@ -5,6 +5,12 @@ from django.contrib import admin
 from django.conf import settings
 import os.path
 
+from django.http import Http404
+from django.http import HttpResponseServerError
+from django.http import HttpResponseForbidden
+from django.core.exceptions import PermissionDenied
+from django.core.exceptions import SuspiciousOperation
+
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -14,12 +20,37 @@ from home.views import redirect_to_home
 
 from articles import urls as article_urls
 from staff import urls as staff_urls
+from globals.views import see404
+
+handler404 = 'globals.views.error404'
+handler500 = 'globals.views.error500'
+handler403 = 'globals.views.error403'
+handler400 = 'globals.views.error400'
+
+
+def fake_404(request):
+    raise Http404
+
+def fake_500(request):
+    raise Exception("oops!")
+
+def fake_403(request):
+    return HttpResponseForbidden()
+
+def fake_400(request):
+    raise SuspiciousOperation
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
 
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
+
+    path('debug_404/',see404),
+    path('404/',fake_404),
+    path('500/',fake_500),
+    path('403/',fake_403),
+    path('400/',fake_400),
 
     path("",redirect_to_home),
     path("home/",include(home_urls),name='home'),
---
+++ dorsal/globals/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from globals import get_global_context
+
+def error404(request, exception):
+    error_code = "404"
+    what_happened = "What you are looking for could not be found. If you reached here by clicking on a link, please come back soon, as this site is still under development"
+    context = {
+        'error_code':error_code,
+        'what_happened':what_happened
+    }
+    context.update(get_global_context())
+
+    return render(request,'globals/error.html', context)
+
+def error500(request):
+    error_code = "500"
+    what_happened = "There was a server error! We're working hard to get it running again!"
+    context = {
+        'error_code':error_code,
+        'what_happened':what_happened
+    }
+    context.update(get_global_context())
+
+    return render(request,'globals/error.html', context)
+
+def error403(request, exception):
+    error_code = "403"
+    what_happened = "You do not have permission to access this, perhaps you need to log in as a staff member?"
+    context = {
+        'error_code':error_code,
+        'what_happened':what_happened
+    }
+    context.update(get_global_context())
+
+    return render(request,'globals/error.html', context)
+
+def error400(request, exception):
+    error_code = "400"
+    what_happened = "This link does not seem to work."
+    context = {
+        'error_code':error_code,
+        'what_happened':what_happened
+    }
+    context.update(get_global_context())
+
+    return render(request,'globals/error.html', context)
+
+def see404(request):
+    error_code = "404"
+    what_happened = "What you are looking for could not be found. If you reached here by clicking on a link, please come back soon, as this site is still under development"
+    context = {
+        'error_code':error_code,
+        'what_happened':what_happened
+    }
+    context.update(get_global_context())
+
+    return render(request,'globals/error.html', context)
