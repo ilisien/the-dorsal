7b0749fbe3103477474f4246a63b3943a5d0dfca
ilisien <ian8lisien@gmail.com>
2023-03-06 22:44:16 -0500

    pronouns + title image captions

--- dorsal/articles/views.py
+++ dorsal/articles/views.py
@@ -4,6 +4,19 @@ import datetime
 from urllib.parse import unquote
 from globals import get_global_context
 
+def image_to_caption(image):
+    if (image.caption != None) and (image.caption != ""):
+        caption_string = f"{image.caption} - "
+    else:
+        caption_string = ""
+    
+    if image.date_taken != None:
+        date_string = f" on {image.date_taken.strftime('%A, %B %-d, %Y').lower()}"
+    else:
+        date_string = ""
+
+    return f'<span class="image-caption">{caption_string}<a href="/staff/{f"{image.photographer.user.first_name.lower()}_{image.photographer.user.last_name.lower()}"}/"> taken by <span class="colored-link">{image.photographer}</span></a>{date_string}</span>'
+
 def article(request, article_id=None, year=None, month=None, day=None, article_title=None):
     if article_id != None:
         article = Article.objects.get(id=article_id)
@@ -18,6 +31,7 @@ def article(request, article_id=None, year=None, month=None, day=None, article_t
 
     author_articles = published_articles.filter(author=article.author).exclude(id=article.id).order_by('-pub_date')[:5]
     context["author_articles"] = author_articles
+    context["title_image_caption"] = image_to_caption(article.title_image)
 
     print(author_articles)
 
